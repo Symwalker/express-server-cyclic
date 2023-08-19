50701bb1bdb0db7e0e8e6d4c00733f679a4f0a92
user name <99411978+Symwalker@users.noreply.github.com>
2023-08-19 08:08:26 +0500

    chnages for * page

--- app.mjs
+++ app.mjs
@@ -3,14 +3,14 @@ import path from "path"
 const app = express()
 const port = 5000
 
+app.use('/', express.static(path.join(process.cwd(),"web","build")));
 app.get('/abc', (req, res) => {
   res.send('Hello World! ' + new Date().toString())
   console.log(req.ip);
 
 })
-
+app.use('*',express.static(path.join(process.cwd(),"web","build")))
 // app.use har trha ke method me chalta he 
-app.use('/', express.static(path.join(process.cwd(),"web","build")));
 // console.log(path.dirname(__filename));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
