70dfdf3580e825a5ebcefb86dd1c91054aa8500d
user name <99411978+Symwalker@users.noreply.github.com>
2023-08-19 07:35:53 +0500

    connecting web with static express app

--- app.mjs
+++ app.mjs
@@ -1,13 +1,17 @@
 import express from 'express'
+import path from "path"
 const app = express()
-const port = process.env.PORT || 5000
+const port = 5000
 
-app.get('/', (req, res) => {
+app.get('/abc', (req, res) => {
   res.send('Hello World! ' + new Date().toString())
   console.log(req.ip);
 
 })
 
+// app.use har trha ke method me chalta he 
+app.use('/', express.static(path.join(process.cwd(),"web","build")));
+// console.log(path.dirname(__filename));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
